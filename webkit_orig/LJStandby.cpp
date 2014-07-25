5963e4730142762eff7161ab0a8ea40d2eb9df87
djstava <djstava@gmail.com>
2014-07-25 17:05:34 +0800

    Base version

---
+++ webkit_orig/LJStandby.cpp
@@ -0,0 +1,20 @@
+#include "LJStandby.h"
+
+namespace WebCore {
+
+LJStandby::LJStandby() 
+{ 
+}
+
+String LJStandby::setStandby() const 
+{ 
+    return "Hello LJStandby.\n"; 
+}
+
+String LJStandby::getStandby() const
+{
+    return "getStandby() called.\n";
+}
+
+} // namespace WebCore
+
