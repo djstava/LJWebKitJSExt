c94c6a323634dd74785d90d3abc781e7dfccdbd8
djstava <djstava@gmail.com>
2014-07-29 11:37:42 +0800

    Add multi method and method with argument for JS object

---
+++ LJStandby.cpp
@@ -0,0 +1,21 @@
+#include "LJStandby.h"
+
+namespace WebCore {
+
+LJStandby::LJStandby() 
+{ 
+}
+
+String LJStandby::setStandbyWithArgu(String value) 
+{ 
+    return value;
+    //return "LJStandby with 1 argument.\n"; 
+}
+
+String LJStandby::setStandby() 
+{ 
+    return "LJStandby with No argument.\n"; 
+}
+
+} // namespace WebCore
+
