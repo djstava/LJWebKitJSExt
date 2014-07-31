d5a2165591ac2e27a7d87e10b5a8041dd92c67e1
djstava <djstava@gmail.com>
2014-07-31 16:17:18 +0800

    Add ljfrontpanel js object

---
+++ LJFrontPanel.cpp
@@ -0,0 +1,27 @@
+#include "LJFrontPanel.h"
+
+
+namespace WebCore {
+
+LJFrontPanel::LJFrontPanel() 
+{ 
+}
+
+bool LJFrontPanel::writeLed(const String& buff)
+{
+    char tmpBuff[5] = {0};
+
+    if((buff.isEmpty()) == true){
+        return false;
+    }
+    else{
+	    printf("tmpBuff=%s\n",buff.utf8().data());
+        strncpy(tmpBuff,buff.utf8().data(),5);
+        return true;
+    }
+}
+
+} // namespace WebCore
+
+
+
---
+++ LJFrontPanel.h
@@ -0,0 +1,29 @@
+#ifndef LJFrontPanel_H 
+#define LJFrontPanel_H
+
+#include <wtf/PassRefPtr.h> 
+#include <wtf/RefCounted.h>
+
+#include <stdio.h>
+#include <string.h>
+#include "CString.h"
+#include "PlatformString.h"
+
+namespace WebCore {
+
+    class LJFrontPanel : public RefCounted<LJFrontPanel> { 
+    public: 
+        static PassRefPtr<LJFrontPanel> create() { return adoptRef(new LJFrontPanel()); }
+        bool writeLed(const String&);
+
+    private: 
+        LJFrontPanel(); 
+    };
+
+} // namespace WebCore
+
+#endif // LJFrontPanel_H
+
+
+
+
